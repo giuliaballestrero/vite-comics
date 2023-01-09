7daa22330685c89dd4a1c848e134f1b9dc669971
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-01-09 17:17:15 +0100

    Add style in Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,26 +1,99 @@
 <script>
+export default {
+    data() {
+      return {
+        navItems: [
+          {
+            text: "Characters",
+            active: false,
+          },
+          {
+            text: "Comics",
+            active: true,
+          },
+          {
+            text: "Movies",
+            active: false,
+          },
+          {
+            text: "Tv",
+            active: false,
+          },
+          {
+            text: "Games",
+            active: false,
+          },
+          {
+            text: "Collectibles",
+            active: false,
+          },
+          {
+            text: "Videos",
+            active: false,
+          },
+          {
+            text: "Fans",
+            active: false,
+          },
+          {
+            text: "News",
+            active: false,
+          },
+          {
+            text: "Shop",
+            active: false,
+          }
+        ]
+      }
+    }
+}
 
 </script>
 
 <template>
   <div class="header-container">
     <div>
-      LOGO
+      <img src="../assets/img/dc-logo.png" alt="lodo dc">
     </div>
 
     <nav>
-      Menu Navigazione
+      <ul>
+        <li v-for="item in navItems" :class="(item.active) ? 'active' : ''">
+        <a href="#">{{ item.text }}</a>
+        </li>
+      </ul>
     </nav>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
  .header-container{
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 80%;
   margin: 0 auto;
-  height: 200px;
-  border: 1px solid black;
  }
+
+ img {
+  padding: 1rem 1rem;
+  width: 80%;
+ }
+
+ nav ul {
+    display: flex;
+
+    li {
+      padding: 1rem 1rem;
+      font-weight: bold;
+      text-transform: uppercase;
+
+      &.active,
+      &:hover {
+        color: $main-color;
+      } 
+      }
+    }
+
 </style>
