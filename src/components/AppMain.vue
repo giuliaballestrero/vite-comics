6a2036586a645b1c8dc77ad03724a9c77eca1351
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-01-10 14:53:35 +0100

    Add new component in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,35 @@
 <script>
+import ComicsComponent from './ComicsComponent.vue';
+
+export default {
+  components: {
+    ComicsComponent,
+  }
+}
 
 </script>
 
 <template>
+    <div class="jumbo-container">
+
+    </div>
     <div class="main-container">
         <div class="my-content">
-            <h2>
-                &RightArrow; Content goes here &LeftArrow;
-            </h2>
+            
+            <ComicsComponent/>
+
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
+
+.jumbo-container {
+    height: 400px;
+    background-image: url(../assets/img/jumbotron.jpg);
+    background-size: cover;
+}
 .main-container {
 background-color: $primary-bg-color;
 }
---
+++ src/components/ComicsComponent.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div>
+        <h1>Nuovo component</h1>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../styles/partials/variables' as *;
+
+</style>
+
+
+
